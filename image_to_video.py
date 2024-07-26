4d3bd959240bd7699ef65f56ecb311149e7c5dc0
fucheng <fucheng@123qiming.com>
2024-07-26 12:22:05 +0800

    chore: Refactor image_to_video.py and text_to_video.py scripts

--- image_to_video.py
+++ image_to_video.py
@@ -1,12 +1,18 @@
 import time
 import requests
 import json
+import base64
 from zhipuai import ZhipuAI
 
 # 硬编码API密钥
-API_KEY = "01eb5c1d13b4f48b1972fc5350fa588c.wbsA6HPHD9vK2jA6"
+API_KEY = "xx"
 
-def generate_video(prompt, image_url):
+def encode_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return encoded_string
+
+def generate_video(prompt, image_path=None, image_url=None):
     # 设置API请求的URL和请求头
     url = "https://open.bigmodel.cn/api/paas/v4/videos/generations"
     headers = {
@@ -15,15 +21,19 @@ def generate_video(prompt, image_url):
     }
     # 设置请求参数
     data = {
-        "model": "cogvideo",
-        "prompt": prompt,
-        "image_url": image_url  # 可选，如果需要图生视频
+        "model": "cogvideoX",
+        "prompt": prompt
     }
+    
+    if image_path:
+        base64_image = encode_image_to_base64(image_path)
+        data["image_url"] = base64_image
+    elif image_url:
+        data["image_url"] = image_url
 
     # 发送POST请求
     response = requests.post(url, headers=headers, data=json.dumps(data))
     response_data = response.json()
-    print(response_data)
     task_id = response_data["id"]
     task_status = response_data["task_status"]
 
@@ -40,8 +50,8 @@ def generate_video(prompt, image_url):
         time.sleep(10)
         get_cnt += 1
 
-    
-
 if __name__ == "__main__":
-    elapsed_time = generate_video('风轻轻吹动她的头发', 
-                                  'https://mmbiz.qpic.cn/sz_mmbiz_png/oHz5prVpwVt8mbiboeicW3pZFZKND8kCXcvECecboFszsReRvBrwuN8RTjrdglXUybz5IjicMbZLkDU6hPkgnxWHQ/640?wxfrom=12&tp=wxpic&wx_fmt=png&amp;from=appmsg')
+    # 使用本地图像路径
+    elapsed_time = generate_video('女人微笑，风吹动头发', image_path='C:/Users/Administrator/Pictures/Saved Pictures/R.jpg')
+    # 或者使用图像URL
+    # elapsed_time = generate_video('美女跳舞，扭动屁股', image_url='https://mmbiz.qpic.cn/sz_mmbiz_png/RcDZlibI6KSBFBlBGNVV2WT9Da060jNTevohT0CUfOoF0730C2V1icRIliaSWJo2diaLibGo9zic6ia1ibs7Hr0wEfhiaOA/640?wxfrom=12&tp=wxpic&usePicPrefetch=1&wx_fmt=png&amp;from=appmsg')
